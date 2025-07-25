3c48ab15eef47f8276d0c1c870fe010fe21ba84b
Francisco Alberto Donat Guijarro <fdonatgu@emeal.nttdata.com>
2025-07-25 08:54:44 +0200

    Cambios visuales 3

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import QuizApp from './App';
 import './index.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
